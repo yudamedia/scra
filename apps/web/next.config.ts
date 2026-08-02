09dd8a6a587b4ba7f8eae9e897a7b48bb26fed9b
Yuda Muyinza <yudamedia@gmail.com>
2026-08-02 04:37:34 +0300

    Add Payload CMS with Postgres adapter, working admin panel

---
+++ apps/web/next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+import { withPayload } from '@payloadcms/next/withPayload'
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default withPayload(nextConfig);
---
+++ apps/web/src/payload.config.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { buildConfig } from 'payload'
+import sharp from 'sharp'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: 'users',
+  },
+  collections: [
+    {
+      slug: 'users',
+      auth: true,
+      fields: [],
+    },
+  ],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
+    },
+  }),
+  sharp,
+})
